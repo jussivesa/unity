a0c287d8935dfa2999c80c3602a0813213ad3d63
jussivesa <jussi.vesa@gmail.com>
2017-03-22 07:40:04 +0200

    Basic gameplay works.

---
+++ SurvivalShooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLenght = 100f;
+
+
+	void Awake() {
+		// init vars
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent <Animator> ();
+		playerRigidbody = GetComponent <Rigidbody> ();
+	}
+
+	void Update() {
+		
+	}
+
+	void FixedUpdate() {
+
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h , v);
+		//MoveForward (h, v);
+
+		Turning ();
+
+		Animating (h, v);
+
+	}
+
+	void MoveForward ( float h, float v ) {
+
+		Vector3 movement = new Vector3(h, 0.0f, v);
+		transform.rotation = Quaternion.LookRotation(movement.normalized);
+
+		movement.Set ( 0f, 0f, v);
+
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Move (float h, float v) {
+		movement.Set (h, 0f, v);
+
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Turning() {
+
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, camRayLenght, floorMask)) {
+			
+			Vector3 playerToMouse = floorHit.point - transform.position;
+
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	
+	}
+
+	void Animating (float h, float v)
+	{
+		// Create a boolean that is true if either of the input axes is non-zero.
+		bool walking = h != 0f || v != 0f;
+
+		// Tell the animator whether or not the player is walking.
+		anim.SetBool ("isWalking", walking);
+	}
+
+}
