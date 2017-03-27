613ed7a79f66ab63ae8b89ea25711fba0b082f3d
jussivesa <jussi.vesa@gmail.com>
2017-03-27 14:57:11 +0300

    Add basic combat, needs work.

---
+++ game-development-project-2017-deadplay/Morbituary/Assets/Code/Actors/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Code.Combat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code.Actors
+{
+    class Enemy : Actor<IAttack>
+	// Use this for initialization
+    {
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+		public static Enemy ToEnemy(GameObject gameObject) {
+			return gameObject.GetComponentInParent<Enemy> ();
+		}
+
+    }
+}
